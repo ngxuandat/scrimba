6abe6d2368367f3dd96cd7308c558d8964980b8e
Dat Ng <datnguyen.academic@gmail.com>
2026-07-04 16:02:57 +0900

    done with settting up servestatic

--- gold-digger/server.js
+++ gold-digger/server.js
@@ -1,10 +1,28 @@
 import http from "node:http";
+import path from "node:path";
+import fs from "node:fs/promises";
+import { getContentType } from "./utils/getContentType.js"
+
 
 
 const PORT = 8000;
 
-const server = http.createServer((req, res) =>{
-    res.end("server up and working")
+const server = http.createServer(async (req, res) =>{
+
+    const __dirname = import.meta.dirname;
+
+    async function serveStatic(req, res, __dirname) {
+        const filePath = path.join(__dirname, "public", req.url === '/'? "index.html" : req.url);
+        const ext = path.extname(filePath);
+        res.setHeader("Content-Type", getContentType(ext));
+        res.statusCode = 200;
+
+        const payload = await fs.readFile(filePath)
+
+        res.end(payload)
+
+    }
+    await serveStatic(req, res, __dirname)
 })
 
 server.listen(PORT, ()=> console.log("Connected on port: ", PORT));
